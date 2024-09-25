d48e122b1fbbfa01858befbfc9723c374c7bad04
salma hamdi <saalmahm@gmail.com>
2024-09-25 17:13:05 +0100

    challenge 2

---
+++ c2.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int n,r,i;
+printf("entrer un nombre:");
+    scanf("%d",&n);
+printf("voila le factoriele: \n");
+    for (int i=n;i>=1;i--){
+        r*=i;
+
+    }
+    printf("%d!= %d  \n",n,i);
+    return 0;
+}
